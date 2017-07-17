dc5cc9e99032be9b23ca1b866711efbe987dc8db
Caio  Resende <kaio0500@hotmail.com>
2017-07-16 21:55:28 -0300

    add app.js

---
+++ app/app.js
@@ -0,0 +1,10 @@
+
+require('angular');
+
+angular.module('TinyCompany', [
+  require('angular-aria'),
+  require('angular-animate'),
+  require('angular-messages'),
+  require('angular-material'),
+  require('@uirouter/angularjs')
+])
