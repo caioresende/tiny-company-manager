dd19284dce4326a44067ceb4ed156bd8d1f2b4c8
Caio  Resende <kaio0500@hotmail.com>
2017-07-17 20:24:39 -0300

    starting the server file

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var COMPANIES_COLLECTION = "companies";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// Rest of server.js code below
