4fc3f6db7a5d169bc1d43137c43fd790d686d411
Caio  Resende <kaio0500@hotmail.com>
2017-07-17 20:21:19 -0300

    create edit and clear fn

--- app/companies/list/ListCompaniesController.js
+++ app/companies/list/ListCompaniesController.js
@@ -5,19 +5,36 @@ module.exports = function() {
   var self = this;
 
   this.company = {
-    _id: '1',
-    name: 'test',
-    email: 'test',
-    phone: 'test',
-    address: 'test',
-    city: 'test',
-    country: 'test',
-    owners: 'test'
+    _id: '',
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    city: '',
+    country: '',
+    owners: ''
   };
 
-  this.editCompany = function(id) {
-    self.editMode = id;
-    self.layoutAlign = 'center start';
+  this.companyCopy = angular.copy(this.company);
+
+  this.clearForm = function(edit) {
+    if (edit) {
+      self.selectedCompany = angular.copy(self.companyCopy);
+    } else {
+      self.company = angular.copy(self.companyCopy);
+    }
+  };
+
+  this.editCompany = function(company) {
+    if (company) {
+      self.editMode = company._id;
+      self.company = angular.copy(company);
+      self.layoutAlign = 'center start';
+    } else {
+      self.editMode = undefined;
+      self.focusedCompany = undefined;
+      self.layoutAlign = '';
+    }
   };
 
   this.companies = [
