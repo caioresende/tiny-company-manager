5721259dbc1a83de03be779ddf42e7732b388cad
Caio  Resende <kaio0500@hotmail.com>
2017-07-17 19:19:44 -0300

    edit ui router to work properly

--- app/app.js
+++ app/app.js
@@ -6,5 +6,10 @@ angular.module('TinyCompany', [
   require('angular-animate'),
   require('angular-messages'),
   require('angular-material'),
-  require('@uirouter/angularjs')
+  require('@uirouter/angularjs').default,
+  require('./core'),
+  require('./companies')
 ])
+
+.config(require('./routes'))
+.config(require('./config'));
