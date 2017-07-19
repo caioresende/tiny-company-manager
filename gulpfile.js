4543d69ff037920abd1c86652c4ea41412eb4e25
Caio  Resende <kaio0500@hotmail.com>
2017-07-19 19:34:23 -0300

    adding new tasks for prod deploy

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,16 @@ var runSequence = require('run-sequence');
 
 gulp.task('watch', require('./tasks/watch'));
 gulp.task('bundle', require('./tasks/bundle'));
+gulp.task('bundle-prod', require('./tasks/bundle-prod'));
 gulp.task('sass', require('./tasks/sass'));
+gulp.task('sass-prod', require('./tasks/sass-prod'));
 gulp.task('sync', require('./tasks/sync'));
+gulp.task('copy-index', require('./tasks/copy'));
 
 gulp.task('default', [], function() {
   return runSequence('sass', 'bundle', 'watch', 'sync');
 });
 
 gulp.task('buildProd', [], function() {
-  return runSequence('sass', 'bundle');
+  return runSequence('sass-prod', 'bundle-prod', 'copy-index');
 });
