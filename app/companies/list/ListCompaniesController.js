9a3378d46b7a713b73f5df0084f11af6941b3164
Caio  Resende <kaio0500@hotmail.com>
2017-07-19 01:01:32 -0300

    fixing clear form method

--- app/companies/list/ListCompaniesController.js
+++ app/companies/list/ListCompaniesController.js
@@ -1,11 +1,10 @@
-module.exports = function(CompaniesService) {
+module.exports = function(CompaniesService, $mdToast) {
 
   'ngInject';
 
   var self = this;
 
   this.company = {
-    _id: '',
     name: '',
     email: '',
     phone: '',
@@ -15,14 +14,15 @@ module.exports = function(CompaniesService) {
     owners: []
   };
 
-  this.data = CompaniesService;
+  this.crudCardFlex = '30';
 
+  this.data = CompaniesService;
   this.companies = [];
 
   var getCompanies = function() {
     self.loading = 'get';
     CompaniesService.getCompanies().then(function(response) {
-      self.loading = 'false';
+      self.loading = false;
       self.companies = response;
     });
   };
@@ -32,18 +32,25 @@ module.exports = function(CompaniesService) {
   this.addCompany = function() {
     self.loading = 'create';
     CompaniesService.createCompany(self.company).then(function(response) {
-      self.clearForm(false);
+      self.clearForm();
+      $mdToast.showSimple('Company created');
+      self.loading = false;
+    }, function() {
+      $mdToast.showSimple('Error! Try again.');
       self.loading = false;
     });
   };
 
-  this.clearForm = function(edit) {
+  this.clearForm = function() {
     self.company = angular.copy(self.companyCopy);
+    self.companyForm.$setPristine(true);
+    self.companyForm.$setUntouched(true);
   };
 
   this.deleteCompany = function(id) {
-    self.loading = 'delete';
+    self.loading = 'delete' + id;
     CompaniesService.deleteCompany(id).then(function(response) {
+      $mdToast.showSimple('Company deleted');
       self.loading = false;
     });
   };
@@ -63,9 +70,10 @@ module.exports = function(CompaniesService) {
   this.updateCompany = function() {
     self.loading = 'update';
     CompaniesService.updateCompany(self.company).then(function(response) {
+      $mdToast.showSimple('Company updated');
       self.loading = false;
       self.editMode = undefined;
-      self.company = angular.copy(self.companyCopy);
+      self.clearForm();
       self.layoutAlign = '';
     });
   };
