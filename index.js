2ebe40f102f46300c8e67e07ddbc8506a2d4638d
Caio  Resende <kaio0500@hotmail.com>
2017-07-20 14:03:27 -0300

    main file to call other parts of the backend

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.use('/', express.static(__dirname + '/dist'));
+app.use('/assets/stylesheets', express.static(path.join(__dirname, '/dist/assets/stylesheets')));
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+app.use(require('./route/route'));
