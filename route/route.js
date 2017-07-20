8c6ea4c953f935d872b30d6ad7e9818e8d2e0955
Caio  Resende <kaio0500@hotmail.com>
2017-07-20 14:08:53 -0300

    missing dash on api urls

--- route/route.js
+++ route/route.js
@@ -11,21 +11,21 @@ express().use(cors());
 /**
  * Router to make a GET and retrieve all companies on database
  */
-router.get('api/companies', cors(), function(req, resp) {
+router.get('/api/companies', cors(), function(req, resp) {
   controller.get(req, resp);
 });
 
 /**
  * Router to make a POST and create a company on database
  */
-router.post('api/companies', cors(), function(req, resp) {
+router.post('/api/companies', cors(), function(req, resp) {
   controller.add(req, resp);
 });
 
 /**
  * Router to make a PUT and update a company on database
  */
-router.put('api/companies/:id', cors(), function(req, resp) {
+router.put('/api/companies/:id', cors(), function(req, resp) {
   var updateDoc = req.body;
 
   delete updateDoc._id;
@@ -35,8 +35,8 @@ router.put('api/companies/:id', cors(), function(req, resp) {
 /**
  * Router to make a DELETE and remove a company on database
  */
-router.delete('api/companies/:id', cors(), function(req, resp) {
+router.delete('/api/companies/:id', cors(), function(req, resp) {
   controller.delete(req, resp);
 });
 
-module.exports = router
+module.exports = router;
